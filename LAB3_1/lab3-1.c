a2273c5997187cd5f319f1569f1088f050eac0fd
Sofya Cheboltasova <96617834+SofyaCheboltasova@users.noreply.github.com>
2022-07-16 03:51:17 +0700

    Create lab3-1.c

---
+++ LAB3_1/lab3-1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <malloc.h>
+
+void swap (int* a, int* b){
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+void QuickS (int *mas, int left, int right)
+{
+    int L = left, R = right;
+    int pivot  = mas[(left + right) / 2];
+
+    while (L <= R) {
+        while (mas[L] < pivot)
+            ++L;
+
+        while (mas[R] > pivot)
+            --R;
+
+        if  (L <= R)
+            swap(&mas[L++], &mas[R--]);
+    }
+
+    if (left < R)
+        QuickS(mas, left, R);
+    if (right > L)
+        QuickS(mas, L, right);
+}
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) {
+        printf("bad input");
+        return 0;
+    }
+    int *a = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++){
+        if (scanf("%d", &a[i]) != 1) {
+            printf("bad input");
+            return 0;
+        }
+}
+
+    QuickS (a, 0, n - 1);
+
+    for (int i = 0; i < n; ++i)
+        printf("%d ", a[i]);
+
+    free(a);
+    return 0;
+}
